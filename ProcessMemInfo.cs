b1bd1f7c356ce8820ded8ac2aa3765bcdf609d2a
easy66 <easy66@live.com>
2020-06-10 22:36:38 +0800

    * 临时提交

--- ProcessMemInfo.cs
+++ ProcessMemInfo.cs
@@ -39,9 +39,15 @@ exit";
         const int MAX_WAIT_TIME = 1000 * 5;
 
         public string mapsStr { get; private set; }
+        /// <summary>
+        /// 综合了 maps 和 showmap 的数据
+        /// </summary>
         public string smapsStr { get; private set; }
         public string showmapStr { get; private set; }
         public string gpuMemMaps { get; private set; }
+
+        public string smapsStr_csv { get; private set; }
+        public string gpuMemMaps_csv { get; private set; }
 
         public ProcessMemInfo(int pid)
         {
@@ -86,5 +92,70 @@ exit";
 
             return sb.ToString();
         }
+
+
+        private struct SmapsLineInfo
+		{
+            public string addrStart;
+            public string addrEnd;
+            public string attr; // rwxp/s， 可读、可写、可运行、私有或共享
+            public string mapOffset;
+            public string deviceNo;
+            public string fileNo;
+            public string mappedFile;
+            public int size;
+            public int rss;
+            public int pss;
+            public int shared_clean;
+            public int shared_dirty;
+            public int private_clean;
+            public int private_dirty;
+            public int referenced;
+            public int anonymous;
+            public int swap;
+            public int kernelPageSize;
+            public int MMUPageSize;
+            public int Locked;
+            public string VmFlags; // rd mr mw me ac 
+
+            /*
+            rd  - readable
+            wr  - writeable
+            ex  - executable
+            sh  - shared
+            mr  - may read
+            mw  - may write
+            me  - may execute
+            ms  - may share
+            gd  - stack segment growns down
+            pf  - pure PFN range
+            dw  - disabled write to the mapped file
+            lo  - pages are locked in memory
+            io  - memory mapped I/O area
+            sr  - sequential read advise provided
+            rr  - random read advise provided
+            dc  - do not copy area on fork
+            de  - do not expand area on remapping
+            ac  - area is accountable
+            nr  - swap space is not reserved for the area
+            ht  - area uses huge tlb pages
+            ar  - architecture specific flag
+            dd  - do not include area into core dump
+            sd  - soft-dirty flag
+            mm  - mixed map area
+            hg  - huge page advise flag
+            nh  - no-huge page advise flag
+            mg  - mergable advise flag
+             */
+		}
+
+		private string GetCsvOfSmaps(string smapsStr)
+		{
+            if (string.IsNullOrEmpty(smapsStr))
+                return null;
+
+            StringBuilder sb = new StringBuilder((int)(smapsStr.Length * 1.5f));
+            return null;
+		}
     }
 }
